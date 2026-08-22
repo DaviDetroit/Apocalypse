80824e38dc6450cad28fd77265c0a64069782418
unknown <davidetroitff11@gmail.com>
2026-08-22 14:08:50 -0300

    Log: log para token

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ async def load_cogs():
                 logger.info(f"Carregado: {module}")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+logger.info(f"TOKEN EXISTE: {TOKEN is not None}")
 
 
 async def main():
