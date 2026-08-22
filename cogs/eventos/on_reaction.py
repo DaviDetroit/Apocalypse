1aef1db832213b157398b869394c1a1df24b4333
unknown <davidetroitff11@gmail.com>
2026-08-22 19:35:42 -0300

    Fix:Melhoria no sistema do on_reaction.py

--- cogs/eventos/on_reaction.py
+++ cogs/eventos/on_reaction.py
@@ -69,7 +69,7 @@ class ReactionEvents(commands.Cog):
             if evaluator_id == author_id:
                 await self._remove_reaction(payload)
                 warning = await channel.send(
-                    f"{discord.Object(id=evaluator_id).mention} "
+                    f"<@{evaluator_id}> "
                     "❌ Você não pode avaliar a própria publicação.",
                     allowed_mentions=discord.AllowedMentions(
                         users=True,
