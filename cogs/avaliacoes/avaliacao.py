9683101787f003a458644c9f5582160256578739
unknown <davidetroitff11@gmail.com>
2026-08-24 15:19:58 -0300

    fix:correção de merge

--- cogs/avaliacoes/avaliacao.py
+++ cogs/avaliacoes/avaliacao.py
@@ -13,13 +13,6 @@ from config.constants import (
 logger = setup_logger()
 
 #Canal
-<<<<<<< HEAD
-CLIPES_JOGOS = 1427044546973405184
-
-EMOJI = "<:1280pxUmbrella_Corporation_logo:1540878040354000966>"
-
-=======
->>>>>>> dev
 URL_REGEX = re.compile(r"https?://[^\s<>]+")
 
 VIDEO_EXTENSIONS = (
