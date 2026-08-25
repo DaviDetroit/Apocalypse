11ab5b5baf83e9d84d577237f5012f085c082478
unknown <davidetroitff11@gmail.com>
2026-08-25 19:08:24 -0300

    fix:canal pet agora funcionando corretamente

--- cogs/pets/pet.py
+++ cogs/pets/pet.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext import commands
 
 from config.constants import CANAL_PET
+
 from services.pet_service import PetService
+
 from cogs.pets.pet_view import PetLikeView
 
 
@@ -16,12 +18,14 @@ class Pet(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
 
-        if message.author.bot or message.channel.id != CANAL_PET:
+        if message.author.bot:
+            return
+
+        if message.channel.id != CANAL_PET:
             return
 
         if not message.attachments:
             return
-
 
         attachment = message.attachments[0]
 
@@ -38,28 +42,20 @@ class Pet(commands.Cog):
         )
 
 
-        # Cria a mensagem do bot primeiro
         pet_message = await message.channel.send(
-            embed=embed
+            embed=embed,
+            view=PetLikeView()
         )
 
 
-        # Salva no banco e pega o ID do pet
-        pet_id = await PetService.criar_pet(
+        await PetService.criar_pet(
             pet_message.id,
             message.author.id,
             attachment.url
         )
 
 
-        # Adiciona o botão depois que possui o ID
-        await pet_message.edit(
-            view=PetLikeView(pet_id)
-        )
-
-
         await message.delete()
-
 
 
 async def setup(bot: commands.Bot):
