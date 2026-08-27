57a587bfc7be5b2bbb2a55e86a61f41fde288fde
unknown <davidetroitff11@gmail.com>
2026-08-27 15:48:12 -0300

    adicionando a mensagem na dev também

--- cogs/eventos/on_message.py
+++ cogs/eventos/on_message.py
@@ -1,15 +1,18 @@
+import discord
+
 from discord.ext import commands
 
 from services.user_service import UserService
+from services.message_service import MessageService
 
 
 class OnMessage(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
 
         if message.author.bot:
             return
@@ -19,8 +22,19 @@ class OnMessage(commands.Cog):
             username=message.author.name
         )
 
+        # Registra a mensagem
+        await MessageService.registrar_mensagem(
+            discord_message_id=message.id,
+            discord_author_id=message.author.id,
+            guild_id=message.guild.id if message.guild else 0,
+            channel_id=message.channel.id,
+            content=message.content,
+            has_attachment=bool(message.attachments),
+            has_embed=bool(message.embeds)
+        )
 
-async def setup(bot):
+
+async def setup(bot: commands.Bot):
     await bot.add_cog(
         OnMessage(bot)
     )
